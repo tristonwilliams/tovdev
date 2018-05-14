b2df0107776e670110f16913b9617845e835be23
tristonwilliams <triston.williams@salesforce.com>
2018-05-14 12:07:54 +1000

    Update mainApp.js

--- mainApp.js
+++ mainApp.js
@@ -4,72 +4,62 @@
 
 let sdk = new window.sfdc.BlockSDK(); //initalize SDK
 
-let mapData = {
-    apiKey: '',
-    mapCentre: 'Sydney, Au',
-    mapHeight: 300,
-    mapWidth: 600,
-    mapZoom: 14,
-    mapType: 'roadmap',
-    mapMarker: {
-        color: '0xff0000',
-        label: 'Sydney, Au'
-    }
+let imgData = {
+   
+    ImageURL: 'http://image.s4.exct.net/lib/fe8f15737c62077a76/m/1/a9836fc9-54dc-434a-a09f-2b2ca88ce146.png',
+    ImgHeight: 300,
+    ImgWidth: 600,
+    Text: 'Text Over Image',
+
 };
 
-// let defaultContent = '<img width="100%" src="https://maps.googleapis.com/maps/api/staticmap?center=London,+UK&zoom=12&scale=1&size=600x300&maptype=roadmap&format=png&visual_refresh=true" alt="Google Map">';
+
 let defaultContent = '<img src="https://dj-gmaps-sfmc-content-nlock.herokuapp.com//dragIcon.png" style="display:block;margin-left:auto;margin-right:auto">';
 
 let saveData = () => {
-    // console.log('Saving data...');
 
-    // mapData.apiKey = document.getElementById('apiKey').value;
-    mapData.mapWidth = document.getElementById('mapWidth').value;
-    mapData.mapHeight = document.getElementById('mapHeight').value;
-    mapData.mapCentre = document.getElementById('mapCentre').value;
-    mapData.mapZoom = document.getElementById('mapZoom').value;
+    imgData.ImgWidth = document.getElementById('ImgWidth').value;
+    imgData.ImgHeight = document.getElementById('ImgHeight').value;
+    imgData.ImageURL = document.getElementById('ImageURL').value;
+    imgData.Text = document.getElementById('Text').value;
 
 
-    sdk.setData(mapData, (data) => {
-        // mapData = data;
-        let content = '<img width="' + mapData.mapWidth + '" src="https://maps.googleapis.com/maps/api/staticmap?center=' + mapData.mapCentre + '&zoom=' + mapData.mapZoom + '&scale=1&size=' + mapData.mapWidth + 'x' + mapData.mapHeight + '&maptype=roadmap&format=png&visual_refresh=true&&markers=size:mid%7Ccolor:' + mapData.mapMarker.color + '%7Clabel:%7C' + mapData.mapCentre + '" alt="Google Map">';
+    sdk.setData(imgData, (data) => {
+        
+        let content = "<table width='100%' border='0' cellspacing='0' cellpadding='0'> <tbody> <tr> <td style='background-repeat:no-repeat;' background='" + imgData.ImageURL + "' bgcolor='#ddf3e9' width='" + imgData.ImgWidth + "' height='" + imgData.ImgHeight + "' valign='top' class='bgresize'> <!--[if gte mso 9]> <v:rect xmlns:v='urn:schemas-microsoft-com:vml' fill='true' stroke='false' style='background-repeat:no-repeat; width:" + imgData.ImgWidth + ";height:" + imgData.ImgHeight + ";'> <v:fill type='tile' src='" + imgData.ImageURL + "' color='#ddf3e9' /> <v:textbox inset='0,0,0,0'> <![endif]--> <div> <table width='100%' border='0' cellspacing='0' cellpadding='0'> <tbody> <tr> <td width='30' align='left' valign='top' style='font-size: 0%;' class='mobile-hidden'></td> <td align='left' valign='top' class='mobile-padding'><table width='100%' border='0' cellspacing='0' cellpadding='0'> <tbody> <tr> <td align='left' valign='top' style='padding-top: 95px;color: #000000; padding-left: 100px; padding-right: 100px; font-size: 48px;' class='padding65'><span class='banner-heading55'><center>" + imgData.Text + "</center></span></td> </tr> </tbody> </table></td> <td width='30' align='left' valign='top' class='mobile-hidden' style='font-size: 0%;'></td> </tr> </tbody> </table> </div> <!--[if gte mso 9]> </v:textbox> </v:rect> <![endif]--> </td> </tr> </tbody> </table>";
 
-        //check for ampscript
+        
         if (content.search('%%') != -1) {
             sdk.setSuperContent(defaultContent, (newSuperContent) => {});
-            // content = defaultContent;
+          
         }
         sdk.setContent(content);
     });
 
-    console.log(JSON.stringify(mapData));
+    console.log(JSON.stringify(imgData));
 }
 
 let fetchData = () => {
 
-    // console.log('Loading data...');
+  
 
     sdk.getData((data) => {
         if (Object.keys(data).length > 0) {
-            mapData = data;
-            // document.getElementById('apiKey').value = mapData.apiKey;
-            document.getElementById('mapWidth').value = mapData.mapWidth;
-            document.getElementById('mapHeight').value = mapData.mapHeight;
-            document.getElementById('mapCentre').value = mapData.mapCentre;
-            document.getElementById('mapZoom').value = mapData.mapZoom;
+            imgData = data;
+      
+            document.getElementById('ImgWidth').value = imgData.ImgWidth;
+            document.getElementById('ImgHeight').value = imgData.ImgHeight;
+            document.getElementById('ImageURL').value = imgData.ImageURL;
+            document.getElementById('Text').value = imgData.Text;
 
-            // console.log('Found data!');
+           
         }
     });
 
-    console.log(JSON.stringify(mapData));
+    console.log(JSON.stringify(imgData));
 }
 
-// sdk.setContent(defaultContent);
 
-// sdk.setSuperContent(defaultContent, (newSuperContent) => {
-//     defaultContent = newSuperContent;
-// });
 
 window.onload = fetchData;
 window.onchange = saveData;
